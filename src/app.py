a8948a40e4679898211e661b0cac4d91ff01dd9e
Matthew Pecarina <mattp@hbci.com>
2020-08-25 23:28:32 -0500

    fix UserWarning: MongoClient opened before fork. Create MongoClient only after forking.

--- src/app.py
+++ src/app.py
@@ -1,15 +1,24 @@
-from datetime import datetime
-from pymongo import MongoClient
-from urllib import parse
 from bson.json_util import dumps
 from bson.objectid import ObjectId
+from datetime import datetime
+from os import environ
+from pymongo import MongoClient
+from urllib import parse
 
-client = MongoClient("mongo", 27017)
-db = client.kubeless
-collection = client.kubeless.events
+DB_HOST = environ.get("DB_HOST") or "mongo"
+DB_PORT = environ.get("DB_PORT") or 27017
+DB_NAME = environ.get("DB_NAME") or "kubeless"
+COLLECTION_NAME = environ.get("COLLECTION_NAME") or "events"
+
+def get_mongo_collection(db_name, collection_name):
+  client = MongoClient(DB_HOST, DB_PORT)
+  db = client[str(db_name)]
+  collection = db[str(collection_name)]
+  return collection
 
 def put_event(event, context):
   data = event["data"]
+  collection = get_mongo_collection(DB_NAME, COLLECTION_NAME)
   query = collection.insert_one(data)
   resp = str(f"data inserted successfully with id: {query.inserted_id}")
   return resp
@@ -25,6 +34,7 @@ def get_event(event, context):
 
   try: 
       object_id = ObjectId(doc_id) or None
+      collection = get_mongo_collection(DB_NAME, COLLECTION_NAME)
       get_doc = collection.find_one({"_id": {"$eq": object_id}})
       doc_by_id = get_doc
   except Exception as e:
